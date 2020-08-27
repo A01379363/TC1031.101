ac2eeca4fbf3aa1acc08bfa744bd176854793b29
A01379363 <57368094+A01379363@users.noreply.github.com>
2020-08-27 14:47:17 -0500

    Add files via upload

--- Recursion/sumVector.cpp
+++ Recursion/sumVector.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <numeric>
 using namespace std;
 
 int vectorSum(vector<int> v)
@@ -12,10 +13,30 @@ int vectorSum(vector<int> v)
     return n + vectorSum(v);
 }
 
+int matrixSum(vector<vector<int>> m)
+{
+    if (m.size() == 0)
+        return 0;
+
+    int n = accumulate(m[m.size() - 1].begin(), m[m.size() - 1].end(), 0);
+    m.pop_back();
+    return n + matrixSum(m);
+}
+
 int main()
 {
     vector<int> v{30, 64, 17, 103, 28};
     int sum = vectorSum(v);
     cout << "Sum of elements in vector: " << sum << endl;
+
+    vector<vector<int>> m{
+        {1, 2, 3},
+        {1, 2, 3},
+        {1, 2, 3},
+        {1, 2, 3},
+    };
+    int sum2 = matrixSum(m);
+    cout << "Sum of elements in matrix: " << sum2 << endl;
+
     return 0;
 }
